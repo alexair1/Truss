e039ceeaa417d4ca753d6e1620ddc7d49c0dbf11
alexair1 <alexair@optusnet.com.au>
2016-01-07 13:25:26 +1100

    a1.1.1
    Bug fixes and improvements
    Added hazer profile

--- Truss/src/Truss/tests.java
+++ Truss/src/Truss/tests.java
@@ -5,9 +5,13 @@ import javax.swing.JOptionPane;
 public class tests {
 	public static void main(String[] args){
 		
-		String format = "%-20s%s\n";
-		System.out.printf(format, "Chicken Burger", "$10.00");
-		System.out.printf(format, "fdg", "$50.00");
+		int phase = 0;
+		int angle = 5;
+		if(angle == 180 || angle == 360){
+			angle = 0;
+		}
+		System.out.println(         255*Math.sin(Math.toRadians(angle ))               );
+
 		
 	}
 }
